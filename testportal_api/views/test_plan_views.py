feaa01a4a9a19e8f43829f6f71faeee25651013f
gagelarsen <gagelarsen53@gmail.com>
2022-05-09 12:21:51 -0600

    add markdown view for testplan

--- testportal_api/views/test_plan_views.py
+++ testportal_api/views/test_plan_views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
+from django.template.defaultfilters import register, stringfilter
+
+import markdown as md
 
 from testportal.models import TestPlan
 
 from testportal_api.serializers import TestPlanSerializer
+
+
+@register.filter()
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
 
 
 class TestPlanList(generics.ListCreateAPIView):

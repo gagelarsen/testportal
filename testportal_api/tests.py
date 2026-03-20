b34630aaca88d3135237973349492c0c8a22cd5c
Gage Larsen <glarsen@aquaveo.com>
2026-03-20 10:01:35 -0400

    fix more tests

--- testportal_api/tests.py
+++ testportal_api/tests.py
@@ -8,14 +8,14 @@ from testportal.models import Suite
 class SuiteTests(APITestCase):
 
     def test_view_suites(self):
-        url = reverse('testportal_api:listcreate')
+        url = reverse('testportal_api:suite_listcreate')
         response = self.client.get(url, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_suite(self):
         data = {'name': 'Suite2', 'active': True}
-        url = reverse('testportal_api:listcreate')
+        url = reverse('testportal_api:suite_listcreate')
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)

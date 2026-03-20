5b50787bc704ebb71cd613c9ec17997d0755efa5
Gage Larsen <glarsen@aquaveo.com>
2026-03-20 11:00:10 -0400

    reliability fixes

--- testportal/models/suite.py
+++ testportal/models/suite.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 
 from testportal.models import Product
@@ -14,8 +13,6 @@ class Suite(models.Model):
     active = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
     product = models.ForeignKey(Product, blank=True, null=True, on_delete=models.SET_NULL)
-
-    objects = models.Manager()  # Default Manager
 
     def __str__(self):
         return f'{self.name}'
